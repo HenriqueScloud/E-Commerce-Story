f62b8d68b22754670ae693b2e33e722d2319288e
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-10-04 20:08:44 -0300

    NEW COMMIT - LOJA

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -4,34 +4,37 @@ import { useContext } from "react";
 import { TodoContext } from "../../../providers/TodoContext";
 
 export const CartItemCard = ({ card }) => {
-  const { delItemCardList } = useContext(TodoContext);
+  const { delItemCardList, addItemCardList, removeQtd } = useContext(TodoContext);
   return (
     <li className={style.CartItemCard}>
-      <div>
-        <img src={card.img} alt={card.title} />
-      </div>
-      <main>
-        <h3 className="font_title_3">{card.title}</h3>
-        <span className="font_body">
-          {card.price.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
+        <span>
+          <div>
+            <img src={card.img} alt={card.title} />
+          </div>
+          <main>
+            <h3 className="font_title_3">{card.title}</h3>
+            <span className="font_body">
+              {card.price.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+          </main>
         </span>
-      </main>
-      <aside className="unico">
-        <button> - </button>
-        <p> 1 </p>
-        <button> + </button>
-      </aside>
-
-      <button className="qtd"
-        aria-label="delete"
-        title="Remover item"
-        onClick={() => delItemCardList(card)}
-      >
-        <MdDelete size={21} />
-      </button>
+      <span className="spanno">
+        <aside className="unico">
+          <button onClick={()=> removeQtd(card)}> - </button>
+          <p> {card.qtd} </p>
+          <button onClick={()=> addItemCardList(card)}> + </button>
+        </aside>
+        <button
+          aria-label="delete"
+          title="Remover item"
+          onClick={() => delItemCardList(card)}
+        >
+          <MdDelete size={21} />
+        </button>
+      </span>
     </li>
   );
 };
